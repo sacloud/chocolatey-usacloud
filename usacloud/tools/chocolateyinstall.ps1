39b10ac026d986088f9b5af23563bcfe30992a7a
DESKTOP-T2PGGH0\223n <223n@223n.tech>
2020-12-02 18:39:55 +0900

    #17 対応 * Profileを作成 * usacloud用のスクリプトを作成 * Profileにusacloudの読み込み処理を追加

--- usacloud/tools/chocolateyinstall.ps1
+++ usacloud/tools/chocolateyinstall.ps1
@@ -22,4 +22,31 @@ $packageArgs = @{
 }
 
 # https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
-Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyZipPackage @packageArgs;
+
+# Create Usacloud Profile
+$profileDir           = "$(Split-Path -Parent $PROFILE)";
+$usacloudProfilePath  = "$(Join-Path $profileDir 'Usacloud.PowerShell_profile.ps1')";
+if (-not (Test-Path $usacloudProfilePath)) {
+  New-Item -Path $usacloudProfilePath -ItemType File -Force;
+}
+
+# Overwrite Profile
+usacloud completion powershell | Out-File $usacloudProfileCheck;
+
+# Create Profile
+if (-not (Test-Path $PROFILE)) {
+  New-Item -Path $PROFILE -ItemType File -Force;
+}
+
+# Append Load Script
+if (-not (Select-String -Path $PROFILE -Pattern "usacloud" -Quiet)) {
+  echo "# Usacloud profile"                                                                                          | Out-File -Append $PROFILE;
+  echo "`$UsacloudProfile = `"`$(Join-Path `$(Split-Path -Parent `$PROFILE) `'Usacloud.PowerShell_profile.ps1`')`";" | Out-File -Append $PROFILE;
+  echo "if (Test-Path(`$UsacloudProfile)) {"                                                                         | Out-File -Append $PROFILE;
+  echo "  . `$UsacloudProfile;"                                                                                      | Out-File -Append $PROFILE;
+  echo "}"                                                                                                           | Out-File -Append $PROFILE;
+}
+
+# Reload profile
+. $PROFILE;
