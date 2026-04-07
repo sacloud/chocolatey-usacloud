bfd2faa04dfca8dc0d72e6af28bc9fb24ac6d9bd
223n <223n@223n.tech>
2026-04-07 09:41:04 +0900

    mod: 存在しない場合は無視されるため、32bit/64bitのアンインストールを呼び出す方式に変更
    Signed-off-by: 223n <223n@223n.tech>

--- usacloud/tools/chocolateyuninstall.ps1
+++ usacloud/tools/chocolateyuninstall.ps1
@@ -1,16 +1,6 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = 'usacloud*'
-  zipFileName   = 'usacloud_windows-386.zip'
-  zipFileName64 = 'usacloud_windows-amd64.zip'
-}
+$packageName = $env:ChocolateyPackageName;
 
-# Only necessary if you did not unpack to package directory - see https://chocolatey.org/docs/helpers-uninstall-chocolatey-zip-package
-$os = Get-WmiObject -Class Win32_OperatingSystem;
-if ($os.OSarchitecture.Contains("64")) {
-  Uninstall-ChocolateyZipPackage -PackageName $packageArgs['packageName'] -ZipFileName $packageArgs['zipFileName64']
-} else {
-  Uninstall-ChocolateyZipPackage -PackageName $packageArgs['packageName'] -ZipFileName $packageArgs['zipFileName']
-}
+Uninstall-ChocolateyZipPackage -PackageName $packageName -ZipFileName 'usacloud_windows-386.zip'
+Uninstall-ChocolateyZipPackage -PackageName $packageName -ZipFileName 'usacloud_windows-amd64.zip'
